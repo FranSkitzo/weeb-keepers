8c7973573c2702655796c886cb6bb87ed6b34d15
Sloan <Sloan.boyce@gmail.com>
2021-11-11 18:20:10 -0500

    added top anime section, cleaned up some code.

--- dev/js/discover.js
+++ dev/js/discover.js
@@ -1,10 +1,13 @@
 let animeSearch = 'https://api.jikan.moe/v3/search/anime?q=';
+let generalApi = 'https://api.jikan.moe/v3/'
 let searchInput = $('#search-input');
 let searchBtn = $('#search-btn');
 let animeList = [];
 let animeListEl = $('#anime-list');
 let animeDisplayed = false;
 
+
+// fetches anime titles directly corresponding to the user input that was provided in searchInput
 searchBtn.on('click', () => {
     refreshAnimeList();
     console.log("click");
@@ -15,30 +18,60 @@ searchBtn.on('click', () => {
         for(let i = 0; i < data.results.length; i++) {
             animeList[i] = data.results[i];
         }
-        displayAnime();
+        createCards(animeList, false);
     }).catch(err => {
         console.log(err);
     });
     
 });
 
-function displayAnime() {
-    for(let i = 0; i < animeList.length; i++) {
-        let animeCard = document.createElement('div');
-        let animeImg = document.createElement('img');
-        let animeTitle = document.createElement('h3');
-        $(animeImg).attr('src', animeList[i].image_url);
-        $(animeTitle).text(animeList[i].title);
-        $(animeCard).addClass('card');
-        $(animeImg).addClass('card-img card');
-        $(animeCard).append(animeImg);
-        $(animeCard).append(animeTitle);
-        $(animeListEl).append(animeCard);
-    }
-}
-
+// refreshes the anime list and clears the old elements off the page 
 function refreshAnimeList() {
     animeList = [];
     $('.card').remove();
     $('.card-img').remove();
 }
+
+// fetches for the top anime that are currently airing
+function getTopAnime() {
+    console.log('getting top anime');
+    fetch(generalApi + 'top/anime/'+1+'/airing')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.top);
+        createCards(data.top, true);
+    });
+}
+
+// createCards function is a boilerplate function that makes generating elements easier
+function createCards(list, isTop) {
+    if(!isTop) {
+        for(let i = 0; i < list.length; i++) {
+            let animeCard = document.createElement('div');
+            let animeImg = document.createElement('img');
+            let animeTitle = document.createElement('h4');
+            $(animeImg).attr('src', list[i].image_url);
+            $(animeTitle).text(list[i].title);
+            $(animeCard).addClass('card');
+            $(animeImg).addClass('card-img card');
+            $(animeCard).append(animeImg);
+            $(animeCard).append(animeTitle);
+            $(animeListEl).append(animeCard);
+        }
+    }else {
+        for(let i = 0; i < 10; i++) {
+            let animeCard = document.createElement('div');
+            let animeImg = document.createElement('img');
+            let animeTitle = document.createElement('h4');
+            $(animeImg).attr('src', list[i].image_url);
+            $(animeTitle).text(list[i].title);
+            $(animeCard).addClass('card');
+            $(animeImg).addClass('card-img card');
+            $(animeCard).append(animeImg);
+            $(animeCard).append(animeTitle);
+            $('.top-anime').append(animeCard);
+        }
+    }
+}
+
+getTopAnime();
