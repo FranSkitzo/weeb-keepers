859539bb14550c5526f6acb0300f4ded8f6d29e9
Sloan <Sloan.boyce@gmail.com>
2021-11-16 21:28:18 -0500

    layout nearly complete, added listener so for each card that is on the screen, when their button is cliicked, it knows what card it is based on their id

--- dev/js/discover.js
+++ dev/js/discover.js
@@ -5,7 +5,7 @@ let searchBtn = $('#search-btn');
 let animeList = [];
 let animeListEl = $('.anime-list');
 let airingListEl = $(".top-airing");
-let tendingAnimeEl = $(".top-anime");
+let trendingAnimeEl = $(".top-anime");
 let animeDisplayed = false;
 
 
@@ -21,6 +21,11 @@ searchBtn.on('click', () => {
             animeList[i] = data.results[i];
         }
         createCards(animeList, false, animeListEl);
+        airingListEl.hide();
+        airingListEl.parent().hide();
+        trendingAnimeEl.hide();
+        trendingAnimeEl.parent().hide();
+        
     }).catch(err => {
         console.log(err);
     });
@@ -40,14 +45,20 @@ $(function() {
         $('.id-' + id).find('.card-info').hide();
         console.log(e.target);
     });
+
+    // on click, get this to save to local storage tomorrow
+    $('.anime-list, .top-anime, .top-airing').on('click', '.panel-btn', (e) => {
+        let id = e.currentTarget.parentNode.parentNode.className.split('id-')[1];
+        console.log(id);
+    });
 });
 
 
 // refreshes the anime list and clears the old elements off the page 
 function refreshAnimeList() {
     animeList = [];
-    $('.card').remove();
-    $('.card-img').remove();
+    $('.panel').remove();
+    //$('.card-img').remove();
 }
 
 // fetches for the top anime that are currently airing
@@ -57,7 +68,7 @@ function getTopAnime() {
     .then(res => res.json())
     .then(data => {
         console.log(data.top);
-        createCards(data.top, true, tendingAnimeEl);
+        createCards(data.top, true, trendingAnimeEl);
     });
 
     fetch(generalApi + 'top/anime/'+1+'/airing')
@@ -72,47 +83,76 @@ function getTopAnime() {
 function createCards(list, isTop, el) {
     if(!isTop) {
         for(let i = 0; i < list.length; i++) {
+            let cardCol = document.createElement('div');
             let animeCard = document.createElement('div');
             let animeImg = document.createElement('img');
             let animeTitle = document.createElement('h4');
-            let animeInfo = document.createElement('div');
-
-            $(animeImg).attr('src', list[i].image_url);
+            let animeInfo =  document.createElement('div');
+            let animeBody = document.createElement('p');
+            let addBtn = document.createElement('button');
+            let moreInfoBtn = document.createElement('button');
+            let animeBodyInfo = '<ul><li> Rated: '+ list[i].rated + '</li> <li> Episodes: '+list[i].episodes.toString() + '</li> <li>Score: '+list[i].score.toString() +'</li></ul>';
+            $(cardCol).addClass('column my-4');
+            //$(animeImg).attr('src', list[i].image_url);
             $(animeTitle).text(list[i].title);
-            $(animeCard).addClass('card id-'+i.toString());
-            $(animeImg).addClass('card-img card');
-            $(animeInfo).addClass('card-info');
-            $(animeInfo).html('<ul> <li>Airing period: ' + list[i].start_date.slice(0, 10) + ' - '
-             + list[i].end_date.slice(0, 10) + '</li> <li> Rated: '+ list[i].rated +
-             '</li> <li> Episodes: '+list[i].episodes.toString() +
-             '</li> <li>Score: '+list[i].score.toString() +'</li> <li>Type: '+list[i].type+
-             '</li> <li> Synopsis: '+list[i].synopsis+'</li></ul>');
+            $(animeTitle).addClass('panel-title');
+            $(animeCard).addClass('panel id-'+i.toString());
+            $(animeCard).css('background-image', 'url('+list[i].image_url+')')
+            //$(animeImg).addClass('card-img');
+            $(animeInfo).addClass('panel-content');
+            $(animeBody).addClass('panel-body');
+            $(addBtn).addClass('panel-btn');
+            $(moreInfoBtn).addClass('panel-btn');
+            $(addBtn).text('Add to List');
+            $(moreInfoBtn).text('More Info');
+            $(animeBody).html(animeBodyInfo);
+            $(animeInfo).append(animeTitle);
+            $(animeInfo).append(animeBody);
+            $(animeInfo).append(addBtn);
+            $(animeInfo).append(moreInfoBtn);
             $(animeCard).append(animeImg);
+            //$(animeCard).append(animeTitle);
             $(animeCard).append(animeInfo);
-            $(animeCard).append(animeTitle);
-            $(el).append(animeCard);
+            $(cardCol).append(animeCard);
+            $(el).append(cardCol);
+            
         }
     }else {
         for(let i = 0; i < list.length; i++) {
+            let cardCol = document.createElement('div');
             let animeCard = document.createElement('div');
             let animeImg = document.createElement('img');
             let animeTitle = document.createElement('h4');
-            let animeRank = document.createElement('h5');
             let animeInfo =  document.createElement('div');
-            $(animeImg).attr('src', list[i].image_url);
-            $(animeTitle).text(list[i].title);
-            $(animeCard).addClass('card id-'+i.toString());
-            $(animeImg).addClass('card-img card');
-            $(animeInfo).addClass('card-info');
-            $(animeInfo).html('<ul> <li>Episodes: '+list[i].episodes+
+            let animeBody = document.createElement('p');
+            let addBtn = document.createElement('button');
+            let moreInfoBtn = document.createElement('button');
+            let animeBodyInfo = '<ul> <li>Episodes: '+list[i].episodes+
             '</li> <li>Rank: '+list[i].rank.toString()+'</li> <li>Score: '+list[i].score.toString()+
-            '</li></ul>');
-            $(animeRank).text('.' + list[i].rank);
-            $(animeCard).append(animeRank);
+            '</li></ul>';
+            $(cardCol).addClass('column my-4');
+            //$(animeImg).attr('src', list[i].image_url);
+            $(animeTitle).text(list[i].title);
+            $(animeTitle).addClass('panel-title');
+            $(animeCard).addClass('panel id-'+i.toString());
+            $(animeCard).css('background-image', 'url('+list[i].image_url+')')
+            //$(animeImg).addClass('card-img');
+            $(animeInfo).addClass('panel-content');
+            $(animeBody).addClass('panel-body');
+            $(addBtn).addClass('panel-btn');
+            $(moreInfoBtn).addClass('panel-btn');
+            $(addBtn).text('Add to List');
+            $(moreInfoBtn).text('More Info');
+            $(animeBody).html(animeBodyInfo);
+            $(animeInfo).append(animeTitle);
+            $(animeInfo).append(animeBody);
+            $(animeInfo).append(addBtn);
+            $(animeInfo).append(moreInfoBtn);
             $(animeCard).append(animeImg);
-            $(animeCard).append(animeTitle);
+            //$(animeCard).append(animeTitle);
             $(animeCard).append(animeInfo);
-            $(el).append(animeCard);
+            $(cardCol).append(animeCard);
+            $(el).append(cardCol);
         }
     }
 }
